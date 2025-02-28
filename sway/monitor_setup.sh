1fbca65f7905f7b518826cfb23ce488786cae72f
Abraham <apalanis@redhat.com>
2025-02-28 11:05:02 -0500

    updates, monitor script

--- sway/monitor_setup.sh
+++ sway/monitor_setup.sh
@@ -2,6 +2,7 @@
 if swaymsg -t get_outputs | grep -q "DP-2"; then
     swaymsg output eDP-1 resolution 1920x1080 position 0 1440 scale 1
     swaymsg output DP-2 resolution 2560x1440 position 0 0 scale 1
-else
-    swaymsg output eDP-1 resolution 1920x1080 position 0 0 scale 1
+elif swaymsg -t get_outputs | grep -q "DP-7"; then
+    swaymsg output eDP-1 resolution 1920x1080 position 2560 0 scale 1
+    swaymsg output DP-7 resolution 2560x1440 position 0 0 scale 1
 fi
