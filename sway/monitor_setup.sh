f0e566d5e99b8b7b3ed09ccc6c9f6a268551dd8d
Abraham <apalanis@redhat.com>
2025-06-06 17:35:21 -0400

    updates

--- sway/monitor_setup.sh
+++ sway/monitor_setup.sh
@@ -5,7 +5,4 @@ if swaymsg -t get_outputs | grep -q "DP-3"; then
 elif swaymsg -t get_outputs | grep -q "HDMI-A-1"; then
     swaymsg output eDP-1 resolution 1920x1080 position 0 1080 scale 1
     swaymsg output HDMI-A-1 resolution 1920x1080 position 0 0 scale 1
-elif swaymsg -t get_outputs | grep -q "DP-7"; then
-    swaymsg output eDP-1 resolution 1920x1080 position 2560 0 scale 1
-    swaymsg output DP-7 resolution 2560x1440 position 0 0 scale 1
 fi
